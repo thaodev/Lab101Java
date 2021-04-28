d1d79f9c94975993bf0f7c55127774c996f0b8e3
Rachel Nguyen <thaontt1408@gmail.com>
2021-04-28 07:18:30 -0600

    Update everything else

--- src/StringTokens.java
+++ src/StringTokens.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class StringTokens {
+import java.util.Scanner;
+
+public class StringTokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        s.trim();
+
+        String[] arrayOfStrings = s.split("[ .,?!'_@]+");
+        if (arrayOfStrings.length >= 1) {
+            System.out.println(arrayOfStrings.length);
+            for (String str : arrayOfStrings) {
+                System.out.println(str);
+            }
+        }
+        else {
+            System.out.println(0);
+        }
+
+        scan.close();
+    }
 }
