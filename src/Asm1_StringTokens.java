486c5481678f8ffab794f29417f7bf13893d4ec3
Rachel Nguyen <thaontt1408@gmail.com>
2021-04-28 21:03:32 -0600

    Add the content for the String token final version

--- src/Asm1_StringTokens.java
+++ src/Asm1_StringTokens.java
@@ -1,3 +1,22 @@
+import java.io.*;
+import java.util.*;
 public class Asm1_StringTokens {
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+        if(s.length() > 0){
+            String[] arrayOfStrings = s.split("[!,?._'@\\s]+");
+            System.out.println(arrayOfStrings.length);
+            for(String str : arrayOfStrings) {
+                System.out.println(str);
+            }
+        }else {
+            System.out.println(0);
+        }
+        // Write your code here.
+        scan.close();
+    }
 }
+
+
+
