f776c01fbcaa0dd20952206c803bfa5cc75629c6
Rachel Nguyen <thaontt1408@gmail.com>
2021-04-28 21:00:24 -0600

    Add stringTokens and substring comparison final version

---
+++ src/Asm1_StringTokens.java
@@ -0,0 +1,3 @@
+public class Asm1_StringTokens {
+
+}
---
+++ src/Asm1_SubstringComparison.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Asm1_SubstringComparison {
+    public static void main(String[] args) {
+        System.out.println("Input a string and an integer: ");
+        Scanner input = new Scanner(System.in);
+        String a =  input.nextLine();
+        int k = input.nextInt();
+        System.out.println(getSmallestAndLargest(a,k));
+    }
+    public static String getSmallestAndLargest(String a, int k) {
+        String currentString = a.substring(0,k);
+        String smallest = currentString;
+        String largest = currentString;
+        for (int i = 1; i <= a.length() - k ; i++) {
+            currentString = a.substring(i, i + k);
+            if(smallest.compareTo(currentString) > 0) {
+                smallest = currentString;
+            }
+            if(largest.compareTo(currentString) < 0) {
+                largest = currentString;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+}
