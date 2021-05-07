ff7e151487bdbc8f24b59862fb407ceecca105c3
Rachel Nguyen <thaontt1408@gmail.com>
2021-05-06 22:08:05 -0600

    Add asm2_Subarray

---
+++ src/Asm2_SubArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Asm2_SubArray {
+    public static int subArraySum(int[] a, int n) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            int sum = 0;
+            for (int j = i; j < a.length; j++) {
+                sum += a[j];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        System.out.println(subArraySum(array, n));
+    }
+}
